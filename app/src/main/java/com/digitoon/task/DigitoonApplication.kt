3265811b6ee9d02d94aaba27c030c284119e840f
hamidreza <hr.shadabkia@gmail.com>
2023-12-07 13:26:37 +0330

    Initial commit

---
+++ app/src/main/java/com/digitoon/task/DigitoonApplication.kt
@@ -0,0 +1,10 @@
+package com.digitoon.task
+
+import android.app.Application
+import timber.log.Timber
+
+class DigitoonApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
